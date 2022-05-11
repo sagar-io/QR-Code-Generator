5a60ac2900cb20798701c34a83e22efd2f893285
Sagar Sharma <tech.sagarsh@gmail.com>
2022-05-11 22:14:12 +0530

    QR generater Created

---
+++ app.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('.qr-text');
+const qrImg = document.querySelector('.qr-img');
+const downloadBtn = document.querySelector('.download-btn');
+
+let userText, link;
+
+function handleQR() {
+    userText = this.value;
+    link =  `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userText}` ;
+    qrImg.src = link;
+}
+
+input.addEventListener('change', handleQR);
+
+downloadBtn.addEventListener('click', () => {
+    saveAs(link, 'img.png');
+});
+
